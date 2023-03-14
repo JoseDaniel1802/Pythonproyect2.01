95242ac4c8aa4b12c35d32aba98d948371e42373
JoseDaniel1802 <124645280+JoseDaniel1802@users.noreply.github.com>
2023-03-14 09:45:54 -0600

    actualizacion
    python nuevo

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 mayor = 0
-menor  = 0
-
-
+residuo  = 0
+n1 = int(input("Ingrese un número"))
+n2 = int(input("Ingrese un número"))
+if n1 > n2:
+    mayor = n1
+elif n2 > n1:
+    mayor = n2
+print("El número mayor es: ",mayor)
