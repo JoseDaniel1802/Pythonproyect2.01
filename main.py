8011c7dad63b201bf0c7ae35f7db7e4865e4dcf9
OscarInteriano <124413716+OscarInteriano@users.noreply.github.com>
2023-03-14 09:49:50 -0600

    Actualización de python
    Se hizo el segundo paso que es verificar si es par o impar

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 mayor = 0
 menor  = 0
 
-
+residuo = mayor // 2
+if residuo == 0:
+    print(f'El número {mayor} es par por que su residuo es 0')
+elif residuo != 0:
+    print(f'El número {mayor} es impar por que su residuo es {residuo}')
+    
